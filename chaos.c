dd800063927fa5df82bd2f49f62d4faa0adf5944
Johnathan Burchill <j.burchill@ucalgary.ca>
2022-03-13 14:48:24 +0000

    a-over-r powers now calculated for potential.

--- chaos.c
+++ chaos.c
@@ -4,6 +4,7 @@
 
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_sf_legendre.h>
+#include <gsl/gsl_math.h>
 
 
 int main ()
@@ -20,6 +21,7 @@ int main ()
 	double phi = 0.;
 
 	double *polynomials = NULL;
+	double *aoverrpowers = NULL;
 	double *times = NULL;
 	double *gnm = NULL;
 	double *hnm = NULL;
@@ -43,11 +45,12 @@ int main ()
 
 	size_t n = gsl_sf_legendre_array_n(maxN);
 	polynomials = malloc(n * sizeof(double));
+	aoverrpowers = malloc(maxN * sizeof(double));
 	times = (double*)malloc(nTimes * sizeof(double));
 	// More memory than needed. Could be revised
 	gnm = (double*)malloc(maxN * (maxN + 2) * nTimes * sizeof(double));
 	hnm = (double*)malloc(maxN * (maxN + 2) * nTimes * sizeof(double));
-	if (polynomials == NULL || times == NULL || gnm == NULL || hnm == NULL)
+	if (polynomials == NULL || aoverrpowers == NULL || times == NULL || gnm == NULL || hnm == NULL)
 	{
 		printf("Cannot remember anything. Check my memory.\n");
 		goto cleanup;
@@ -84,28 +87,30 @@ int main ()
 	// {
 	// 	printf("t = %lf\n", times[i]);
 	// }
-	gRead = 0;
-	hRead = 0;
-	for (int l = 1; l <= maxN; l++)
-	{
-		for (int m = 0; m <= l; m++)
-		{
-			printf("g_%d_%d = %lf\n", l, m, gnm[gRead*nTimes + 0]);
-			gRead++;
-			if (m > 0)
-			{
-				printf("h_%d_%d = %lf\n", l, m, hnm[hRead*nTimes + 0]);
-				hRead++;
-			}
-		}
-	}
+	// gRead = 0;
+	// hRead = 0;
+	// for (int l = 1; l <= maxN; l++)
+	// {
+	// 	for (int m = 0; m <= l; m++)
+	// 	{
+	// 		printf("g_%d_%d = %lf\n", l, m, gnm[gRead*nTimes + 0]);
+	// 		gRead++;
+	// 		if (m > 0)
+	// 		{
+	// 			printf("h_%d_%d = %lf\n", l, m, hnm[hRead*nTimes + 0]);
+	// 			hRead++;
+	// 		}
+	// 	}
+	// }
 	fclose(f);
 	f = NULL;
 
 	double x = 0;
 	double magneticPotential = 0.0;
+	double magneticPotentialN = 0.0;
 
 	// 2 Hz simulated
+	double aoverr = 1.0;
 	for (double t = 0.; t < 86400.; t+=0.5)
 	{
 		status = gsl_sf_legendre_array(GSL_SF_LEGENDRE_SCHMIDT, maxN, cos(theta), polynomials);
@@ -114,27 +119,37 @@ int main ()
 			printf("status: %s\n", gsl_strerror(status));
 			goto cleanup;
 		}
+		aoverr = a / r;
+		aoverrpowers[0] = aoverr * aoverr; // (a/r)^n+1, n starting at 1
+		for (int i = 2; i <= maxN; i++)
+		{
+			aoverrpowers[i-1] = aoverrpowers[i-2] * aoverr;
+		}
 
 		magneticPotential = 0.0;
 		gRead = 0;
 		hRead = 0;
 		for (int l = 1; l <= maxN; l++)
 		{
-			magneticPotential += gnm[gRead*nTimes] * polynomials[gsl_sf_legendre_array_index(l, 0)];
+			magneticPotentialN = gnm[gRead*nTimes] * polynomials[gsl_sf_legendre_array_index(l, 0)];
 			gRead++;
 			for (int m = 1; m <= l; m++)
 			{
-				magneticPotential += (gnm[gRead*nTimes + 0] * cos(phi) + hnm[hRead*nTimes + 0] * sin(phi) )* polynomials[gsl_sf_legendre_array_index(l, m)];
+				magneticPotentialN += (gnm[gRead*nTimes + 0] * cos(phi) + hnm[hRead*nTimes + 0] * sin(phi) ) * polynomials[gsl_sf_legendre_array_index(l, m)];
 				gRead++;
 				hRead++;
 			}
+			magneticPotentialN *= aoverrpowers[l-1] * a;
 		}
-		magneticPotential *= a;
+		magneticPotential += magneticPotentialN;
 	}
+	for (int i = 1; i <= maxN; i++)
+		printf("aoverr[%d]: %lf\n", i, aoverrpowers[i-1]);
 	printf("magneticPotential(r=%lf, theta=%lf, phi=%lf) = %lf\n", r, theta, phi, magneticPotential);
 
 cleanup:
 	if (polynomials != NULL) free(polynomials);
+	if (aoverrpowers != NULL) free(aoverrpowers);
 	if (times != NULL) free(times);
 	if (gnm != NULL) free(gnm);
 	if (hnm != NULL) free(hnm);
